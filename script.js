3fbabc1206678add3b11b34901bf5d001c01b104
ronitdalal27 <dalalronit131@gmail.com>
2025-01-15 12:43:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-function updateTimer{
+function updateTimer(){
 	const timer = document.getElementById("timer");
 	const date = new Date();
 	
@@ -7,11 +7,11 @@ function updateTimer{
 	const month = String(date.getMonth()+1).padStart(2,"0"); //bcoz Date() function takes month from 0 so +1 and padstart is to make single month look 07 instead of 7
 	const day = String(date.getDate()).padStart(2,"0");
 	const hours = date.getHours();
-	const formatedHours = hours%12 || 12;
-	const minutes = String(date.getminutes()).padStart(2,"0");
+	const formatedHours = hours % 12 || 12;
+	const minutes = String(date.getMinutes()).padStart(2,"0");
 	const seconds = String(date.getSeconds()).padStart(2,"0");
 	const ampm = hours>=12 ? "PM" : "AM";
-	const formatedTime = `${month}/${day}/${year},${formatedHours}:${minutes}:${seconds}${ampm}`;
+	const formatedTime = `${month}/${day}/${year}, ${formatedHours}:${minutes}:${seconds} ${ampm}`;
 
 	timer.textContent = formatedTime;
 }
